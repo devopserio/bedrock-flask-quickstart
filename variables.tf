7d3ed14af6ecc4e52594d30fb2ecb425bd685404
Liat Hoffman <devopserhq@Mac-2.lan>
2025-01-08 15:12:26 +0200

    adapting the code to accept a vpc id and subnet tags

--- variables.tf
+++ variables.tf
@@ -3,21 +3,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr_1" {
-  description = "The CIDR block for the first public subnet"
-  default     = "10.0.1.0/24"
-}
-
-variable "public_subnet_cidr_2" {
-  description = "The CIDR block for the second public subnet"
-  default     = "10.0.2.0/24"
-}
-
 variable "your_ip_address" {
   description = "Your local IP address in the format of x.x.x.x"
 }
@@ -38,12 +23,11 @@ variable "subdomain" {
   description = "The subdomain for the application in route53"
 }
 
-variable "openai_api_key" {
-  description = "The OpenAI API key"
-  sensitive   = true
-}
-
 variable "flask_secret_key" {
   description = "The Flask secret key"
   sensitive   = true
+}
+
+variable "vpc_id" { 
+  description = "The VPC ID"
 }
